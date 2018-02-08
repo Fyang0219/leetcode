fcacefac05c10fd56f11ab9355517224d94d6ca0
Fei Yang <feiiyang0219@gmail.com>
2018-02-07 23:27:54 -0500

    updated mergetree

--- mergetree.py
+++ mergetree.py
@@ -1,9 +1,10 @@
 def mergeTree(self,t1, t2):
-    
+    #t1 and t2 are tree nodes
     if t1 is None:
         return t2
     if t2 is None:
         return t1
+
     t1.val += t2.val 
     t1.left = self.mergeTree(t1.left, t2.left)
     t1.right = self.mergeTree(t1.right, t2.right)
